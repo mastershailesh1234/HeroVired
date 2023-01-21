3598f95b92f288465565211ca026575861f5a70f
Shailesh Patro <shailesh.patro2002@gmail.com>
2023-01-21 17:28:52 +0530

    navigation modification

---
+++ javascript array assignment/hide.js
@@ -0,0 +1,60 @@
+function nav1() {
+  var el = document.getElementsByClassName("question");
+  for (var i = 0; i < el.length; i++) {
+    if (i == 0) {
+      el[i].style.display = "flex";
+    } else {
+      el[i].style.display = "none";
+    }
+  }
+}
+function nav2() {
+  var el = document.getElementsByClassName("question");
+  for (var i = 0; i < el.length; i++) {
+    if (i == 1) {
+      el[i].style.display = "flex";
+    } else {
+      el[i].style.display = "none";
+    }
+  }
+}
+function nav3() {
+  var el = document.getElementsByClassName("question");
+  for (var i = 0; i < el.length; i++) {
+    if (i == 2) {
+      el[i].style.display = "flex";
+    } else {
+      el[i].style.display = "none";
+    }
+  }
+}
+function nav4() {
+  var el = document.getElementsByClassName("question");
+  for (var i = 0; i < el.length; i++) {
+    if (i == 3) {
+      el[i].style.display = "flex";
+    } else {
+      el[i].style.display = "none";
+    }
+  }
+}
+function nav5() {
+  var el = document.getElementsByClassName("question");
+  for (var i = 0; i < el.length; i++) {
+    if (i == 4) {
+      el[i].style.display = "flex";
+    } else {
+      el[i].style.display = "none";
+    }
+  }
+}
+function nav6() {
+  var el = document.getElementsByClassName("question");
+  for (var i = 0; i < el.length; i++) {
+    if (i == 5) {
+      el[i].style.display = "flex";
+    } else {
+      el[i].style.display = "none";
+    }
+  }
+}
