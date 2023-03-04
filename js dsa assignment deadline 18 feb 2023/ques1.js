4eec1c55f321a9e6272234de36769a4d9591dca7
Shailesh Patro <shailesh.patro2002@gmail.com>
2023-03-04 15:21:06 +0530

    assignment

---
+++ js dsa assignment deadline 18 feb 2023/ques1.js
@@ -0,0 +1,14 @@
+//reverse a linkedlist
+
+class Node{
+    constructor(value){
+        this.value=value;
+        this.next=null;
+    }
+}
+class linkedlist{
+    constructor(value){
+        this.head=null;
+    }
+
+}
---
+++ stack and queue assignment/ques1.js
@@ -0,0 +1,20 @@
+// Q1. Student A took Student B's Laptop and started playing with him by changing his password. 
+// B requested A to reveal the password, but A doesn't want to end the fun by revealing the password straightaway, 
+// so he asked B to select a number N. Then he gave a set of N numbers, stating that if he takes the modulo of, a 
+// sum of the given numbers with the smallest prime number from the given set of numbers if exists, else by finding 
+// the smallest prime divisor of sum, would give him the age of his loved ones. So spinning the given numbers that 
+// many times in an anticlockwise direction he will get the password for his device.
+
+// Example:
+
+// N = 6
+
+// Input: 90 45 37 80 1 46
+
+// Output: 80 1 46 90 45 37
+
+// N = 5
+
+// Input: 90 45 78 27 63
+
+// Output: 27 63 90 45 78
