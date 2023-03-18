52125bfa3070fddf8fea44c46c3dc33ad3994b8a
Shailesh Patro <shailesh.patro2002@gmail.com>
2023-03-18 19:23:37 +0530

    exercise 2

--- Understanding JSON, HTML Requests And AJAX/exercise1/index.js
+++ Understanding JSON, HTML Requests And AJAX/exercise1/index.js
@@ -1,7 +1,12 @@
-<restaurant
-  name="yellow chilli"
-  address="new delhi"
-  lat="47.608940"
-  lng="-122.340141"
-  type="sitdown"
-/>;
+import { xml2json } from "xml-js";
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const json = xml2json(this.response, { spaces: 2, compact: true });
+    }
+  };
+  xhttp.open("GET", "data.xml", true);
+  xhttp.send();
+}
+loadDoc();
