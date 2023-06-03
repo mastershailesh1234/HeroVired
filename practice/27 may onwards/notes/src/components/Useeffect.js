5690eda29f42e3000acb41e643455c5e9ffb9a19
Shailesh Patro <shailesh.patro2002@gmail.com>
2023-06-03 19:52:45 +0530

    react product page done

--- practice/27 may onwards/notes/src/components/Useeffect.js
+++ practice/27 may onwards/notes/src/components/Useeffect.js
@@ -1,35 +1,50 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../CSS/products.css";
 const Useeffect = () => {
   console.log(1);
   let a = 10;
   let ele1 = document.getElementById("header");
   const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
   //   dummyjson.com/products
-  https: console.log(ele1);
+  console.log(ele1);
   useEffect(() => {
-    let ele2 = document.getElementById("header");
     const getdata = async () => {
-      let response = await axios.get("https://dummyjson.com/products");
-      console.log(response.data);
+      await axios.get("https://dummyjson.com/products").then((res) => {
+        console.log(res.data);
+        setData(res.data.products);
+      });
     };
     getdata();
+  }, []);
+
+  useEffect(() => {
+    let ele2 = document.getElementById("header");
     console.log(ele2);
     console.log(2);
   }, [count]);
   console.log(3);
   return (
-    <div>
-      Useeffect {a}
-      <div id="header">Hello</div>
-      <button
+    <div className="container">
+      {/* <button
         onClick={() => {
           setCount(count + 1);
         }}
       >
         +
       </button>
-      <h1>{count}</h1>
+      <h1>{count}</h1> */}
+      {data &&
+        data.map((ele) => {
+          return (
+            <div className="products">
+              <h1>{ele.title}</h1>
+              {/* <p>{ele.description}</p> */}
+              <img src={ele.thumbnail} alt="" />
+            </div>
+          );
+        })}
     </div>
   );
 };
