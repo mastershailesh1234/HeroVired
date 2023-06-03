6e86f1dc0f61343dab1c0d5f6c93a60cf12e5f8f
Shailesh Patro <shailesh.patro2002@gmail.com>
2023-06-03 18:53:11 +0530

    useeffect dependcy array and axios fetch data

--- practice/27 may onwards/notes/src/components/Useeffect.js
+++ practice/27 may onwards/notes/src/components/Useeffect.js
@@ -1,20 +1,35 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Useeffect = () => {
   console.log(1);
   let a = 10;
   let ele1 = document.getElementById("header");
-  console.log(ele1);
+  const [count, setCount] = useState(0);
+  //   dummyjson.com/products
+  https: console.log(ele1);
   useEffect(() => {
     let ele2 = document.getElementById("header");
+    const getdata = async () => {
+      let response = await axios.get("https://dummyjson.com/products");
+      console.log(response.data);
+    };
+    getdata();
     console.log(ele2);
     console.log(2);
-  }, []);
+  }, [count]);
   console.log(3);
   return (
     <div>
       Useeffect {a}
       <div id="header">Hello</div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <h1>{count}</h1>
     </div>
   );
 };
